ab4a2943f5e67233989c3816f0c7f08cb359815f
Srijit Sen <125910970+srijit07@users.noreply.github.com>
2024-07-12 14:05:34 +0530

    Add files via upload
    As part of my Web Development and Design Internship, I successfully completed the required tasks within the one-month time frame. The first task required extensive use of HTML5 and CSS, but I also added a bit of JavaScript to enhance the functionality and make the project more dynamic. The second task took more time than expected, but the end result was fruitful, and I was able to accomplish all of my tasks within the given time frame.

---
+++ Task 1/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() 
+{
+    const yearSpan = document.getElementById('year');
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+});
